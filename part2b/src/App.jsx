4b9ad8953b707b3e4663a7cd51f53cf4b68ed18b
Xander Varga <xxandervaragd@xanders-mbp.mynetworksettings.com>
2024-09-06 18:00:37 -0400

    CLOSE

--- part2b/src/App.jsx
+++ part2b/src/App.jsx
@@ -1,17 +1,51 @@
 import { useState } from "react";
-import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [filterString, setFilterString] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (persons.map((person) => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+  };
+
+  const filter = (person) => person.name.includes(filterString);
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      <h2>Phonebook</h2>
+      filter: <input onChange={(e) => setFilterString(e.target.value)} />
+      <h2>add a new</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.filter(filter).map((person, i) => {
+        return (
+          <div key={person.name + i}>
+            {person.name} {person.number}
+          </div>
+        );
+      })}
     </div>
   );
 };
