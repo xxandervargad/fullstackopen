b4ca6bce8948d897953ba8c409e1784ffcab121a
Xander Varga <xxandervaragd@Xanders-MacBook-Pro.local>
2024-09-08 17:47:35 -0400

    im tired

--- part2b/src/App.jsx
+++ part2b/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getAll, update, add, remove } from "./service/persons.js";
 
-const Person = ({ person }) => {
-  console.log(person);
-
+const Person = ({ person, handleDelete }) => {
   return (
     <div>
-      {person.name} {person.number}
+      {person.name} {person.number}{" "}
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 };
@@ -33,40 +33,72 @@ const AddForm = ({ handleSubmit }) => {
   );
 };
 
-const PersonList = ({ list, filterString }) => {
+const PersonList = ({ list, filterString, handleDelete }) => {
   console.log("person list");
   const filter = (person) => person.name.includes(filterString);
-  const map = (person, i) => <Person key={person + i} person={person} />;
+  const map = (person, i) => (
+    <Person
+      key={person.id}
+      person={person}
+      handleDelete={() => handleDelete(person.id)}
+    />
+  );
   return list.filter(filter).map(map);
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
   const [filterString, setFilterString] = useState("");
+  const [persons, setPersons] = useState([]);
 
-  const handleSubmit = (e, newName, newNumber) => {
+  const handleAdd = (e, newName, newNumber) => {
     e.preventDefault();
+    const temp = { name: newName, number: newNumber };
     if (persons.map((person) => person.name).includes(newName)) {
+      const person = persons.find((p) => p.name === newName);
+      if (person.number !== newNumber) {
+        if (window.confirm("Person already exists. replace number?")) {
+          update(person.id, temp).then((newPerson) =>
+            setPersons(persons.map((p) => (p === person ? newPerson : p)))
+          );
+        }
+        return;
+      }
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons(persons.concat({ name: newName, number: newNumber }));
+    add(temp);
+    setPersons(persons.concat(temp));
   };
+
+  const handleDelete = (id) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete ${
+          persons.find((p) => p.id === id).name
+        }`
+      )
+    ) {
+      remove(id);
+    }
+    setPersons(persons.filter((p) => p.id !== id));
+  };
+
+  useEffect(() => {
+    getAll().then((data) => setPersons(data));
+  }, []);
 
   return (
     <div>
       <h2>Phonebook</h2>
       filter: <input onChange={(e) => setFilterString(e.target.value)} />
       <h2>add a new</h2>
-      <AddForm handleSubmit={handleSubmit} />
+      <AddForm handleSubmit={handleAdd} />
       <h2>Numbers</h2>
-      <PersonList list={persons} filterString={filterString} />
+      <PersonList
+        list={persons}
+        filterString={filterString}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
