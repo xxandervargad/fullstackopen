bbd2ff011a5bf8bac70f532b71c9dfe17da62e57
Xander Varga <xxandervaragd@Xanders-MacBook-Pro.local>
2024-10-04 15:32:43 -0400

    did a lot lol

--- part2b/src/App.jsx
+++ part2b/src/App.jsx
@@ -1,18 +1,56 @@
 import { useState, useEffect } from "react";
 import { getAll, update, add, remove } from "./service/persons.js";
 
+const ErrorMessage = ({ message }) => {
+  const errorStyle = {
+    color: "red",
+    background: "lightgrey",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  };
+  if (message === null) {
+    return null;
+  }
+  return (
+    <div style={errorStyle} className="error">
+      {message}
+    </div>
+  );
+};
+
+const SuccessMessage = ({ message }) => {
+  const successStyle = {
+    color: "green",
+    background: "lightgrey",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  };
+  if (message === null) {
+    return null;
+  }
+  return (
+    <div style={successStyle} className="error">
+      {message}
+    </div>
+  );
+};
+
 const Person = ({ person, handleDelete }) => {
   return (
     <div>
       {person.name} {person.number}{" "}
-      <button onClick={handleDelete}>Delete</button>
+      <button onClick={() => handleDelete(person.id)}>Delete</button>
     </div>
   );
 };
 
 const AddForm = ({ handleSubmit }) => {
-  console.log("add form");
-
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -34,14 +72,10 @@ const AddForm = ({ handleSubmit }) => {
 };
 
 const PersonList = ({ list, filterString, handleDelete }) => {
-  console.log("person list");
-  const filter = (person) => person.name.includes(filterString);
+  const filter = (person) =>
+    person.name.toLowerCase().includes(filterString.toLowerCase());
   const map = (person, i) => (
-    <Person
-      key={person.id}
-      person={person}
-      handleDelete={() => handleDelete(person.id)}
-    />
+    <Person key={person.id} person={person} handleDelete={handleDelete} />
   );
   return list.filter(filter).map(map);
 };
@@ -49,6 +83,10 @@ const PersonList = ({ list, filterString, handleDelete }) => {
 const App = () => {
   const [filterString, setFilterString] = useState("");
   const [persons, setPersons] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+
+  const NOTIF_TIMEOUT = 2000;
 
   const handleAdd = (e, newName, newNumber) => {
     e.preventDefault();
@@ -57,20 +95,42 @@ const App = () => {
       const person = persons.find((p) => p.name === newName);
       if (person.number !== newNumber) {
         if (window.confirm("Person already exists. replace number?")) {
-          update(person.id, temp).then((newPerson) =>
-            setPersons(persons.map((p) => (p === person ? newPerson : p)))
-          );
+          update(person.id, temp)
+            .then((newPerson) => {
+              handleSuccess("Person Updated Successfully.");
+              setPersons(persons.map((p) => (p === person ? newPerson : p)));
+            })
+            .catch(() => handleError("Person Failed to Update"));
         }
         return;
       }
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    add(temp);
-    setPersons(persons.concat(temp));
+    add(temp)
+      .then(() => {
+        handleSuccess("Person Added Successfully.");
+        setPersons(persons.concat(temp));
+      })
+      .catch(() => handleError("Person Failed to Add"));
+  };
+
+  const handleError = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, NOTIF_TIMEOUT);
+  };
+
+  const handleSuccess = (message) => {
+    setSuccessMessage(message);
+    setTimeout(() => {
+      setSuccessMessage(null);
+    }, NOTIF_TIMEOUT);
   };
 
   const handleDelete = (id) => {
+    console.log("deleting", id);
     if (
       window.confirm(
         `Are you sure you want to delete ${
@@ -78,7 +138,9 @@ const App = () => {
         }`
       )
     ) {
-      remove(id);
+      remove(id)
+        .then(() => handleSuccess("Person Deleted Successfully."))
+        .catch(() => handleError("Person Failed to Detele"));
     }
     setPersons(persons.filter((p) => p.id !== id));
   };
@@ -92,6 +154,8 @@ const App = () => {
       <h2>Phonebook</h2>
       filter: <input onChange={(e) => setFilterString(e.target.value)} />
       <h2>add a new</h2>
+      <ErrorMessage message={errorMessage} />
+      <SuccessMessage message={successMessage} />
       <AddForm handleSubmit={handleAdd} />
       <h2>Numbers</h2>
       <PersonList
